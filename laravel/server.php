361a108c0471448f86a3b7ac14c9ed93306fde8a
shiromegane <shiromegane42@gmail.com>
2020-09-25 16:55:00 +0900

    with syntac error

--- laravel/server.php
+++ laravel/server.php
@@ -7,8 +7,9 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+// syntax error
 $uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+    parse_url($_SERVER['REQUEST_URI'];, PHP_URL_PATH)
 );
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
