f07d8213c084a08ee7f3a7ab4029a2f170407fc1
shiromegane <shiromegane42@gmail.com>
2020-09-25 20:33:09 +0900

    test

--- laravel/server.php
+++ laravel/server.php
@@ -19,4 +19,4 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
 }
 
 // with violation of the rules
-     require_once __DIR__.'/public/index.php';
+     require_once __DIR__. '/public/index.php';
